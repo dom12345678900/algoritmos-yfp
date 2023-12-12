3261b6e95023c3e8b201a2a25c86a8cc4e386db6
dom12345678900 <151813285+dom12345678900@users.noreply.github.com>
2023-12-12 11:17:18 -0500

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,44 @@
+#Ejercicio 1 suma dos numeros enteros
+s1= 12
+s2= 9
+s3= (s1 + s2)
+print (s3)
+# Resta dos numeros de punto flotante
+r1= 45.34
+r2=4.3
+r3= (r1 - r2)
+print(r3)
+#Multiplica dos numeros enteros
+m1= 34
+m2=98
+m3= (m1 * m2)
+print(m3)
+#Divide dos numeros de punto flotante
+d1= 45.7
+d2= 42.1
+d3= (d1 / d2)
+print(d3)
+#Ejercicio 2 declara una variable de tipo string que contenga tu nombre completo
+nom= "Domenica Abigail Torres Bonete"
+#Utiliza la funcion len para obtener la longitud de tu nombre completo y guardalo en una variable
+long = len (nom)
+#Imprime tu nombre completo y su longitud utilizando la funcion print
+#Esto nos permitira conocer cuantas letras hay en nuestros nombres y apellidos
+print(nom)
+print(long)
+#Ejercicio 3 declara una variable entera y otra de punto flotante
+ve=60
+vpf=23.6
+#Realiza la conversion de la variable entera a punto flotante y viceversa
+float (ve)
+int(vpf)
+#Imprime en pantalla el tipo de dato original y el tipo de dato despues de la conversion
+print(ve)
+print(vpf)
+#Imprime variables ya convertidas
+#Se observa nuestras variables ya convertidas lo que dara como resultado en la parte de abajo despues de dar run main
+print(float(ve))
+print(int(vpf))
+#Escribe comentarios explicativos en tu codigo para cada seccion importante
+#Importar el codigo a github
+
